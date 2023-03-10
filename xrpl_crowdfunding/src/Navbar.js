c0ddf9c1df9ec18431c6607cd89881d1ddb6c2f4
mathieudupontr <mathieudupont.r@gmail.com>
2023-03-10 00:45:01 -0800

    navbar

---
+++ xrpl_crowdfunding/src/Navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <a href="/" className="XRPL Crowdfunding">
+                XRPL Crowdfunding
+            </a>
+            <ul>
+                <li>
+                    <a href="/Home">Home</a>
+                    <a href="/How it works">How it works</a>
+                    <a href="/Login">Login</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
